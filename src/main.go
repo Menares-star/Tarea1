b7ed66c8ce2e1ff31feaed96b157c5393ae85671
Martín Menares <martin.menares@sansano.usm.cl>
2020-11-15 19:25:32 -0300

    Test

--- src/main.go
+++ src/main.go
@@ -4,35 +4,10 @@ import (
 	"log"
 	"net"
 	//"fmt"
-	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	pb "github.com/Menares-star/Tarea1/src/Mensajes"
+	"github.com/Menares-star/Tarea1/src/Mensajes"
 )
 
-type Server struct{
-}
-
-var codsum int32 =0
-
-	func (s*Server) ReceivedOrden(ctx context.Context, message *Orden)(*Orden, error){
-		codsum++
-		log.Printf("Received message body from client: %s", message.Id)
-		return &Orden{Id :message.Id,
-		Producto:message.Producto,
-		Valor:message.Valor,
-		Tienda:message.Tienda,
-		Destino:message.Destino,
-		Prioridad:message.Prioridad,
-		Codigo:codsum,
-		Tipo:message.Tipo}, nil
-	}
-
-	func (s*Server) ReceivedSeguimiento(ctx context.Context, message *Seguimiento)(*Seguimiento, error){
-		log.Printf("Received message seguimiento from client: %d", message.Codigo)
-		return &Seguimiento{Codigo:message.Codigo,
-		Estado:"En Bodega",
-		}, nil
-	}
 
 func main() {
 
@@ -41,13 +16,13 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := Server{}
+	s := ordenes.Server{}
 
 	grpcServer := grpc.NewServer()
 
 	//REGISTRO DE SERVICIOS
-	RegisterOrdenServiceServer(grpcServer, &s)
-	RegisterSeguimientoServiceServer(grpcServer, &s)
+	ordenes.RegisterOrdenServiceServer(grpcServer, &s)
+	ordenes.RegisterSeguimientoServiceServer(grpcServer, &s)
 	//FIN REGISTRO DE SERVICIOS
 
 
